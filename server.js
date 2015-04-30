b062673b4e80e17597ba03ae5cdff2554cd6ee63
Rene Rosa <realpasro09@hotmail.com>
2015-04-29 22:59:24 -0600

    change server

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-/// <reference path="../../typings/node/node.d.ts"/>
 var Hapi = require('hapi');
+
+// Create a server with a host and port
 var server = new Hapi.Server();
-var routes = require('./routes');
-require('./handlers');
-var plugins = [{
-    register: require('hapi-auth-cookie')
-}];
 server.connection({
-    port: 7203,
-    routes: { cors: true }
+    host: 'localhost',
+    port: 8000
 });
-server.register(plugins, function (err) {
-    if (err) {
-        throw err;
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function (request, reply) {
+       reply('hello world');
     }
-    server.auth.strategy('session', 'cookie', {
-        password: '12390812ASDASD90A8S90DASDAS;;123129381293128390128ASJDHASKDA12231893129',
-        cookie: 'caredforCookie',
-        isSecure: false
-    });
-    server.route(routes);
-    server.start(function () {
-        console.log('info', 'Server running at ', server.info.uri);
-        console.log('info', 'Responding to...');
-        routes.forEach(function (route) {
-            console.log('info', route.method + ' ' + route.path);
-        });
-    });
 });
+
+// Start the server
+server.start();
