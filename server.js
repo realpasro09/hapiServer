426f0739194da23aeed7bf663264779de07ea10b
Rene Rosa <realpasro09@hotmail.com>
2015-04-30 00:22:33 -0600

    adding change

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+/// <reference path="../../typings/node/node.d.ts"/>
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+var routes = require('./routes');
+require('./handlers');
+var plugins = [{
+    register: require('hapi-auth-cookie')
+}];
+server.connection({
+    port: 7203,
+    routes: { cors: true }
 });
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+server.register(plugins, function (err) {
+    if (err) {
+        throw err;
+    }
+    server.auth.strategy('session', 'cookie', {
+        password: '12390812ASDASD90A8S90DASDAS;;123129381293128390128ASJDHASKDA12231893129',
+        cookie: 'caredforCookie',
+        isSecure: false
+    });
+    server.route(routes);
+    server.start(function () {
+        console.log('info', 'Server running at ', server.info.uri);
+        console.log('info', 'Responding to...');
+        routes.forEach(function (route) {
+            console.log('info', route.method + ' ' + route.path);
+        });
+    });
 });
